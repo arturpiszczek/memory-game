a9b2f6f49382f893493fc7ac5fd06e8a13caa4f0
arturpiszczek <a.piszczek24@gmail.com>
2021-09-14 17:33:19 +0200

    Add MainHeader component

---
+++ src/components/Layout/MainHeader.js
@@ -0,0 +1,37 @@
+import { NavLink } from 'react-router-dom';
+
+import classes from './MainHeader.module.css';
+
+const MainHeader = () => {
+  return (
+    <header className={classes.header}>
+      <h1>Memory Game</h1>
+      <nav>
+        <ul>
+        <li>
+            <NavLink activeClassName={classes.active} to="/home">
+              Start
+            </NavLink>
+          </li>          
+          <li>
+            <NavLink activeClassName={classes.active} to="/about">
+              O Grze
+            </NavLink>
+          </li>
+          <li>
+            <NavLink activeClassName={classes.active} to="/ranking">
+              Ranking
+            </NavLink>
+          </li>
+          <li>
+            <NavLink activeClassName={classes.active} to="/contact">
+              Kontakt
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default MainHeader;
