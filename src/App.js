ebd6d80c7d493de0faba3284a44665c201d2bd97
arturpiszczek <a.piszczek24@gmail.com>
2021-09-14 17:51:50 +0200

    Add routing on the page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,36 @@
-function App() {
-  return <div>Hello!</div>;
-}
+import { Route, Switch, Redirect } from 'react-router-dom';
+
+import MainHeader from './components/Layout/MainHeader';
+import Home from './pages/Home';
+import About from './pages/About';
+import Ranking from './pages/Ranking';
+import Contact from './pages/Contact';
+
+const App = () => {
+  return (
+    <div>
+      <MainHeader />
+      <main>
+        <Switch>
+          <Route path="/" exact>
+            <Redirect to="/home" />
+          </Route>
+          <Route path="/home">
+            <Home />
+          </Route>          
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/ranking">
+            <Ranking />
+          </Route>
+          <Route path="/contact">
+            <Contact />
+          </Route>
+        </Switch>
+      </main>
+    </div>
+  );
+};
 
 export default App;
