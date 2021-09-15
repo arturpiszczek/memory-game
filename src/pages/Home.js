03bcb0920d0e84d46b357741daf4c06d9b3c4069
arturpiszczek <a.piszczek24@gmail.com>
2021-09-15 15:05:54 +0200

    Change Home component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,15 @@
+import classes from './Home.module.css';
+
 const Home = () => {
-  return <div>Home page</div>;
+  return (
+    <section className={classes.intro}>
+      <article>
+        <p>Zagraj w nową przeglądarkową grę online.</p>
+        <p>Przekonaj się jak szybko uda Ci się rozwiązać zagadkę!</p>
+      </article>
+      <button>Rozpocznij grę</button>
+    </section>
+  );
 };
 
 export default Home;
