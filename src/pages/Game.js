0f7dea3184a46df49955c55a47905089a309d426
arturpiszczek <a.piszczek24@gmail.com>
2021-09-17 14:05:57 +0200

    Add logic to Game component

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,10 +1,22 @@
+import { useState } from 'react';
+
 import ChooseGame from '../components/ChooseGame';
+import GamePuzzles from '../components/GamePuzzles';
 import classes from './Game.module.css';
 
 const Game = () => {
+  const [isChosen, setIsChosen] = useState(false);
+  const [chosenAmount, setChosenAmount] = useState();
+
+  const setAmountHandler = amount => {
+    setIsChosen(true);
+    setChosenAmount(amount);
+  };
+
   return (
     <section className={classes.game}>
-      <ChooseGame />
+      {!isChosen && <ChooseGame onChoose={setAmountHandler} />}
+      {isChosen && <GamePuzzles elements={chosenAmount} />}
     </section>
   );
 };
