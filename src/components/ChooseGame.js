04e8281da445f3b9540c725df4c8704271185822
arturpiszczek <a.piszczek24@gmail.com>
2021-09-17 14:11:09 +0200

    Add function to ChooseGame component

--- src/components/ChooseGame.js
+++ src/components/ChooseGame.js
@@ -1,12 +1,16 @@
 import classes from './ChooseGame.module.css';
 
-const ChooseGame = () => {
+const ChooseGame = (props) => {
+  const chooseAmountHandler = event => {
+    props.onChoose(event.target.name);
+  };
+
   return (
     <section className={classes.choose}>
       <h2>Wybierz liczbę kart:</h2>
-      <button>12 par</button>
-      <button>18 par</button>
-      <button>24 pary</button>
+      <button onClick={chooseAmountHandler} name='24'>12 par</button>
+      <button onClick={chooseAmountHandler} name='36'>18 par</button>
+      <button onClick={chooseAmountHandler} name='48'>24 pary</button>
     </section>
   );
 };
