adb1b7b63d371720b0b7800a8e8ad31af4e5d4ca
arturpiszczek <a.piszczek24@gmail.com>
2021-09-22 13:39:56 +0200

    Add rotatePuzzleHandler to GamePuzzle component

--- src/components/GamePuzzle.js
+++ src/components/GamePuzzle.js
@@ -1,9 +1,18 @@
+import { useState } from 'react';
+
 import classes from './GamePuzzle.module.css';
 
 const GamePuzzle = (props) => {
+  const [isClicked, setIsClicked] = useState(false);
+
+  const rotatePuzzleHandler = () => {
+    setIsClicked(true);
+    props.onCompare(props.id, props.number);
+  };
+
   return (
-    <div className={classes.puzzle}>
-      <div>{props.number}</div>
+    <div className={`${classes.puzzle} ${isClicked ? classes.clicked : ''} ${!props.visibility ? classes.hide : ''}`} onClick={rotatePuzzleHandler}>
+      {isClicked ? props.number : '?'}
     </div>
   );
 };
