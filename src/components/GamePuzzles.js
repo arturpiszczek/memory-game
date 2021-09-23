6a5699111a0fbb83a28b3e73fd92c5cab2becc3e
arturpiszczek <a.piszczek24@gmail.com>
2021-09-23 15:07:03 +0200

    Add puzzle checking to GamePuzzles component

--- src/components/GamePuzzles.js
+++ src/components/GamePuzzles.js
@@ -8,14 +8,14 @@ const initialPuzzles = [];
 const createPuzzles = (amountOfPuzzles) => {
   for (let i = 0; i < 2; i++) {
     for (let j = 0; j < amountOfPuzzles / 2; j++) {
-      initialPuzzles.push({ value: j, id: Math.random().toFixed(5), isVisible: true });
+      initialPuzzles.push({ value: j, id: Math.random().toFixed(5), isVisible: true, reverse: false });
     }
   }
 };
 
 const GamePuzzles = (props) => {
   createPuzzles(props.elements); 
-  const [puzzles, setPuzzles] = useState(initialPuzzles);
+  const [puzzles, setPuzzles] = useState(initialPuzzles);  
 
   const clickedPuzzles = [];
   const clickedPuzzlesIndex = [];
@@ -36,15 +36,25 @@ const GamePuzzles = (props) => {
     });
 
     clickedPuzzlesIndex.push(index);
-    clickedPuzzles.push(num);
+    clickedPuzzles.push(num);    
 
-    if(clickedPuzzles[0] === clickedPuzzles[1]) {
+    if(clickedPuzzles[0] === clickedPuzzles[1] && clickedPuzzlesIndex[0] !== clickedPuzzlesIndex[1]) {
       setPuzzles((prevPuzzles) => {
         prevPuzzles[clickedPuzzlesIndex[0]].isVisible = false;
         prevPuzzles[clickedPuzzlesIndex[1]].isVisible = false;
         return [...prevPuzzles];
       })
-    }    
+    }
+    
+    if(clickedPuzzles.length === 2){
+      if(clickedPuzzles[0] !== clickedPuzzles[1]) {        
+        setPuzzles((prevPuzzles) => {
+          prevPuzzles[clickedPuzzlesIndex[0]].reverse = Math.random().toFixed(3);
+          prevPuzzles[clickedPuzzlesIndex[1]].reverse = Math.random().toFixed(3);
+          return [...prevPuzzles];
+        })      
+      }      
+    }
   };  
 
   return (
@@ -55,6 +65,7 @@ const GamePuzzles = (props) => {
           id={puzzle.id}
           number={puzzle.value} 
           visibility={puzzle.isVisible}
+          reverse={puzzle.reverse}
           onCompare={comparePuzzlesHandler}
         />
       ))}
