890806215889d46b79c652405702f919bcf4ae2b
arturpiszczek <a.piszczek24@gmail.com>
2021-09-28 15:12:17 +0200

    Add EndGame component

---
+++ src/components/EndGame.js
@@ -0,0 +1,11 @@
+import classes from './EndGame.module.css';
+
+const EndGame = () => {
+  return (
+    <div className={classes.message}>
+      <p>Gratulacje! Udało Ci się ukończyć grę w czasie</p>
+    </div>
+  );
+};
+
+export default EndGame;
