224273cba35e921c45b63f0bd36b460b3792e01f
arturpiszczek <a.piszczek24@gmail.com>
2021-10-02 15:40:46 +0200

    Change GameTimer component

--- src/components/GameTimer.js
+++ src/components/GameTimer.js
@@ -1,28 +1,33 @@
 import { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
+import { timeActions } from '../store/index';
 import classes from './GameTimer.module.css';
-
-let minutes = 0;
 
 const GameTimer = () => {
   const [seconds, setSeconds] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+
+  const status = useSelector(state => state.status.isFinished);
+  const dispatch = useDispatch();
+
+  if (status) {            
+    dispatch(timeActions.addTime({ minutes: minutes, seconds: seconds }));      
+  }  
 
   useEffect(() => {
     const timer = setTimeout(() => {  
-      setSeconds(prevSeconds => {
-        return prevSeconds + 1;
-      });
+      setSeconds(prevSeconds => prevSeconds + 1);
     }, 1000);     
     
-    return () => clearTimeout(timer);
-    
+    return () => {
+      clearTimeout(timer);                 
+    };    
   }, [seconds]);
 
   if (seconds === 60) {    
-    setSeconds(prevSeconds => {
-      return prevSeconds - 60;
-    });    
-    minutes++;
+    setSeconds(prevSeconds => prevSeconds - 60);  
+    setMinutes(prevMinutes => prevMinutes + 1);  
   }   
 
   return (
