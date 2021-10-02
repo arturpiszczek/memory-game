a229d78d34c70ff627592200b927a2b0c55c5c29
arturpiszczek <a.piszczek24@gmail.com>
2021-10-02 15:58:00 +0200

    Change EndGame component

--- src/components/EndGame.js
+++ src/components/EndGame.js
@@ -1,9 +1,27 @@
+import { useSelector } from 'react-redux';
+
 import classes from './EndGame.module.css';
 
 const EndGame = () => {
+  const minutes = useSelector(state => state.time.minutes);
+  const seconds = useSelector(state => state.time.seconds);
+
+  let message;
+
+  switch (minutes) {
+    case 0:
+      message = `Gratulacje! Udało Ci się ukończyć grę w czasie ${seconds} sekund!`;
+      break; 
+    case 1:
+      message = `Gratulacje! Udało Ci się ukończyć grę w czasie ${minutes} minuty i ${seconds} sekund!`;
+      break; 
+    default:
+      message = `Gratulacje! Udało Ci się ukończyć grę w czasie ${minutes} minut i ${seconds} sekund!`;        
+  }
+
   return (
     <div className={classes.message}>
-      <p>Gratulacje! Udało Ci się ukończyć grę w czasie</p>
+      {message}
     </div>
   );
 };
