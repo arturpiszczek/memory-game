6f52c255ebc67397ea4bcbaa902abe7500a5b3d6
arturpiszczek <a.piszczek24@gmail.com>
2021-10-04 19:05:20 +0200

    Add info about username to EndGame component

--- src/components/EndGame.js
+++ src/components/EndGame.js
@@ -5,18 +5,19 @@ import classes from './EndGame.module.css';
 const EndGame = () => {
   const minutes = useSelector(state => state.time.minutes);
   const seconds = useSelector(state => state.time.seconds);
+  const username = useSelector(state => state.username.username);
 
   let message;
 
   switch (minutes) {
     case 0:
-      message = `Gratulacje! Udało Ci się ukończyć grę w czasie ${seconds} sekund!`;
+      message = `Gratulacje ${username}! Udało Ci się ukończyć grę w czasie ${seconds} sekund!`;
       break; 
     case 1:
-      message = `Gratulacje! Udało Ci się ukończyć grę w czasie ${minutes} minuty i ${seconds} sekund!`;
+      message = `Gratulacje ${username}! Udało Ci się ukończyć grę w czasie ${minutes} minuty i ${seconds} sekund!`;
       break; 
     default:
-      message = `Gratulacje! Udało Ci się ukończyć grę w czasie ${minutes} minut i ${seconds} sekund!`;        
+      message = `Gratulacje ${username}! Udało Ci się ukończyć grę w czasie ${minutes} minut i ${seconds} sekund!`;        
   }
 
   return (
