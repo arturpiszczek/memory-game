5a8d24fa568225c12bbf914cd082d33db2cb8b43
arturpiszczek <a.piszczek24@gmail.com>
2021-10-04 18:59:38 +0200

    Change Game component

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,5 +1,7 @@
 import { useState } from 'react';
+import { useSelector } from 'react-redux';
 
+import UserForm from '../components/UserForm';
 import ChooseGame from '../components/ChooseGame';
 import GameTimer from '../components/GameTimer';
 import GamePuzzles from '../components/GamePuzzles';
@@ -9,7 +11,10 @@ import classes from './Game.module.css';
 const Game = () => {
   const [isChosen, setIsChosen] = useState(false);
   const [chosenAmount, setChosenAmount] = useState(0);
-  const [isFinished, setIsFinished] = useState(false);  
+  const [isFinished, setIsFinished] = useState(false); 
+  
+  const enteredUsername = useSelector(state => state.username.username);
+  const isNameEntered = enteredUsername !== '';
 
   const setAmountHandler = amount => {
     setIsChosen(true);
@@ -19,10 +24,11 @@ const Game = () => {
   const setGameOverHandler = () => {
     setIsFinished(true);
   };
-
+  
   return (
     <section className={classes.game}>
-      {!isChosen && <ChooseGame onChoose={setAmountHandler} />}
+      {!isNameEntered && <UserForm />}
+      {(isNameEntered && !isChosen) && <ChooseGame onChoose={setAmountHandler} />} 
       {(isChosen && !isFinished) && <GameTimer />}
       {(isChosen && !isFinished) && <GamePuzzles elements={chosenAmount} onGameOver={setGameOverHandler} />}
       {isFinished && <EndGame />}
