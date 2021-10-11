d0b3d54fce71f1a07c0d77f2404bca73f8aaa891
arturpiszczek <a.piszczek24@gmail.com>
2021-10-11 17:24:35 +0200

    Change Contact component

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,5 +1,23 @@
+import classes from './Contact.module.css';
+
 const Contact = () => {
-  return <div>Contact page</div>;
+  return (
+    <section className={classes.contact}>
+      <h2>Skontaktuj się ze mną poprzez:</h2>
+      <article>
+        <i className="devicon-google-plain"></i>
+        a.piszczek24@gmail.com
+      </article>
+      <article>
+        <i className="devicon-linkedin-plain"></i>        
+        <a target="_blank" rel="noreferrer" href="https://linkedin.com/in/arturpiszczek">konto na LinkedIn</a>        
+      </article>
+      <article>
+        <i className="devicon-github-original"></i>        
+        <a target="_blank" rel="noreferrer" href="https://github.com/arturpiszczek">konto na GitHub</a>        
+      </article>
+    </section>
+  );
 };
 
 export default Contact;
